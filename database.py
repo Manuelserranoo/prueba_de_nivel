a20d064e20b8bfcd8e1ca48e3a6b80c75317a8b8
Manoloserranoo <manuel.serranovallejo@usp.ceu.es>
2023-03-06 18:32:25 +0100

    cambios en database

--- database.py
+++ database.py
@@ -1,6 +1,25 @@
-class Vehiculo():
-    def __init__(self, color, ruedas):
+class Vehiculo:
+    def __init__(self, VIN, color, ruedas):
         self.color = color
         self.ruedas = ruedas
+        self.VIN = VIN
     def __str__(self):
         return "color {}, {} ruedas".format( self.color, self.ruedas )
+    
+class Vehiculos:
+    lista = []
+    @staticmethod
+    def buscar(VIN):
+        for vehiculo in Vehiculos.lista:
+            if vehiculo.VIN == VIN:
+                return vehiculo
+    @staticmethod
+    def agregar(VIN,color,ruedas):
+        vehiculo = Vehiculo(VIN,color,ruedas)
+        Vehiculos.lista.append(vehiculo)
+    @staticmethod
+    def modificar(VIN,color,ruedas):
+        vehiculo = Vehiculos.buscar(VIN)
+        if vehiculo != None:
+            vehiculo.color = color
+            vehiculo.ruedas = ruedas
